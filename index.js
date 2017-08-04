ef41d53b8d93ecdd1766e3a442a70f962e15526c
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-08-05 00:11:01 +0300

    post hello world :smile:

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const Twit = require('twit');
 require('dotenv').config();
 
-const bot = new Twit({
+let bot = new Twit({
   consumer_key: process.env.CONSUMER_KEY,
   consumer_secret: process.env.CONSUMER_SECRET,
   access_token: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
   timeout_ms: 60 * 1000,
 });
+
+bot.post(
+  'statuses/update',
+  {
+    status: 'hello world!',
+  },
+  (err, data, response) => {
+    if (err) console.error(err);
+    else console.log(`${data.text} was tweeted.`);
+  }
+);
